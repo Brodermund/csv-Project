e90979d28e86339972601e4abb0d33b7157d0453
Brodermund <rodermundbobby@yahoo.com>
2021-09-22 10:15:12 -0400

    mmds

---
+++ partcheck.js
@@ -0,0 +1,10 @@
+const partMapper = (sku) => partTable[sku] || false;
+const partTable = {
+    "PAR-DWKIT1":true,
+    "PAR-EDRYERKIT":true,
+    "PAR-IM72SS":true,
+    "PAR-BSSPSB1090":true,
+    "PAR-ERANGEKIT":true,
+    "PAR-CLF72WM2PK":true,
+    "PAR-AQU1472":true,
+}
